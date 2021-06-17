8d6f09b9b5130c2d8ccbdad34dfe468731b8c4ab
gcpexamples <vineetsemwal1982@gmail.com>
2021-06-17 11:16:05 +0530

    minor improvement

--- appengine/bucketdemo/src/main/java/com/example/bootdemo/BucketController.java
+++ appengine/bucketdemo/src/main/java/com/example/bootdemo/BucketController.java
@@ -5,18 +5,20 @@ import com.google.cloud.ReadChannel;
 import com.google.cloud.storage.Storage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.ByteBuffer;
 
-@RestController("/files")
+@RequestMapping("/files")
+@RestController
 public class BucketController {
 
     @Autowired
     private Storage storage;
 
-    @GetMapping("/names")
-    public String fetchContent() throws Exception{
+    @GetMapping("/trainees")
+    public String fetchTrainees() throws Exception{
         StringBuilder builder = new StringBuilder();
         try(ReadChannel channel=storage.reader("vinproject7-securedbucket","names.txt")){
             ByteBuffer buffer= ByteBuffer.allocate(32*1024);
