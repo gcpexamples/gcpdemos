46b3f673030c9dcb57ced217a35c1d8eba5e42f4
gcpexamples <vineetsemwal1982@gmail.com>
2021-06-17 11:39:03 +0530

    converted from nio to simple io for understanding

--- appengine/bucketdemo/src/main/java/com/example/bootdemo/BucketController.java
+++ appengine/bucketdemo/src/main/java/com/example/bootdemo/BucketController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 
 @RequestMapping("/files")
 @RestController
@@ -18,15 +22,17 @@ public class BucketController {
     private Storage storage;
 
     @GetMapping("/trainees")
-    public String fetchTrainees() throws Exception{
+    public String fetchTrainees() throws Exception {
         StringBuilder builder = new StringBuilder();
-        try(ReadChannel channel=storage.reader("vinproject7-securedbucket","names.txt")){
-            ByteBuffer buffer= ByteBuffer.allocate(32*1024);
-            int readBytes;
-            while((readBytes=channel.read(buffer))>0){
-                buffer.flip();
-                String data=new String(buffer.array(),0,readBytes);
-                builder.append(data);
+        try (ReadChannel channel = storage.reader("vinproject7-securedbucket", "names.txt")) {
+            InputStream input = Channels.newInputStream(channel);
+            BufferedInputStream bin = new BufferedInputStream(input);
+            byte[] buffer = new byte[64 * 1000];
+            int readCount;
+            while ((readCount = bin.read(buffer)) > 0) {
+                System.out.println("read bytes="+readCount);
+                String readString = new String(buffer,0,readCount);
+                builder.append(readString);
             }
         }
         return builder.toString();
