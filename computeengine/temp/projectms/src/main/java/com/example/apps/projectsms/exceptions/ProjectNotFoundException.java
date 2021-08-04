6581351c836197e1baea71557a9ce9ee855ed7e4
gcpexamples <vineetsemwal1982@gmail.com>
2021-08-04 18:51:58 +0530

    temp removed

--- computeengine/temp/projectms/src/main/java/com/example/apps/projectsms/exceptions/ProjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.apps.projectsms.exceptions;
-
-public class ProjectNotFoundException extends RuntimeException{
-
-    public ProjectNotFoundException(String msg){
-        super(msg);
-    }
-}
