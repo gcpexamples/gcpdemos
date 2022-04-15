4cfb4e350d4b474b8807daceb5c1939f58b5f21b
vineetsemwal <vineetsemwal82@gmail.com>
2022-04-15 16:05:31 +0530

    springbootdemo pdated for maven

--- docker/java-helloworld/src/App.java
+++ docker/java-helloworld/src/App.java
@@ -7,7 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello trainees, Today is the first day of docker" );
+        System.out.println( "Hello mulesoft batch10 trainees, Today is the first day of docker" );
     }
 }
 
---
+++ docker/myjava-example/Vineet.java
@@ -0,0 +1,13 @@
+
+/**
+ * Hello world!
+ *
+ */
+public class Vineet
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Vineet" );
+    }
+}
+
