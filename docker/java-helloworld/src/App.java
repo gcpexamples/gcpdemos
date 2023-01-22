0d7855ce00015099a542b47ab2b4ba9bce80d315
vineetsemwal <vineetsemwal82@gmail.com>
2023-01-22 11:41:05 +0530

    useless dockerfile ind ocker example removed

--- docker/java-helloworld/src/App.java
+++ docker/java-helloworld/src/App.java
@@ -7,7 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Docker, Dec 20" );
+        System.out.println( "Welcome to Docker, jan 22" );
     }
 }
 
