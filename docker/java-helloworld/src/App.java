95d036628b87c5b22a6b0459a9ecc17302e81084
vineetsemwal <vineetsemwal82@gmail.com>
2023-02-13 17:22:25 +0530

    env demo+ entrypoint demo+ simple dockerfile for tomcat in bootemo inside docker

--- docker/java-helloworld/src/App.java
+++ docker/java-helloworld/src/App.java
@@ -7,7 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Docker, jan 22" );
+        System.out.println( "Welcome to Docker, 13 feb" );
     }
 }
 
